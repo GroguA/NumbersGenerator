cd11fc6ab945806f23a4afb0944220c346295fb5
GroguA <117662071+GroguA@users.noreply.github.com>
2025-05-18 20:40:18 +0300

    create project

---
+++ NumbersGenerator/NumbersGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersGeneratorApp.swift
+//  NumbersGenerator
+//
+//  Created by Александра Сергеева on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NumbersGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
