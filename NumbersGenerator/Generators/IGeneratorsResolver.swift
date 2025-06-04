d9d701d42916ae979733275bc68a6e28d8bb360e
GroguA <117662071+GroguA@users.noreply.github.com>
2025-06-04 17:15:38 +0300

    refactor generators resolver

--- NumbersGenerator/Generators/IGeneratorsResolver.swift
+++ NumbersGenerator/Generators/IGeneratorsResolver.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 protocol IGeneratorsResolver {
-    func getNumbers() -> [Int]
+    func getNumbers() -> [Int]?
+    func setCurrentType(_ type: NumberType)
     func reset()
+    func addGenerator(for type: NumberType, generator: INumberGenerator)
 }
